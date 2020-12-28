170a1eaa553fdc1bb0c17f0e648a774b99317f4d
Matluba <mzohidova@mail.ru>
2020-12-28 22:31:16 +0300

    Custom Video Player html refactor

--- custom_video_player/script.js
+++ custom_video_player/script.js
@@ -16,20 +16,34 @@ function toggleVideoStatus () {
 //Update play/pause icon
 function updatePlayIcon () {
     if (video.paused) {
-        play.innerHTML = 'i class="fa fa-play fa-2x"></i>';
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
     } else {
-        play.innerHTML = 'i class="fa fa-pause fa-2x"></i>';
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
     }
 }
 
 //Update progress and timestamp
 function updateProgress () {
-    return true;
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    // Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    // Get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 //Set video time to progress
 function setVideoProgress () {
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 //Stop video
@@ -38,13 +52,11 @@ function stopVideo () {
     video.pause();
 }
 
-
-
 //Event listeners
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('play', updatePlayIcon);
 video.addEventListener('pause', updatePlayIcon);
-video.addEventListener('timeupdate', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 
