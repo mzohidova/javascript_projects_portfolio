90c23191a0fd75f7992d77a594c9ec5f7d1399d6
Matluba <mzohidova@mail.ru>
2021-01-23 20:24:50 +0300

    Music Player - JS(1) - cannot find mistakes, the IDE cannot read controls and HTML

---
+++ music_player_REMASTER_it/script.js
@@ -0,0 +1,89 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+//Music
+const songs = [
+    {
+        name: 'brand_new_day',
+        displayName: 'Brand New Day',
+        artist: 'Ryan Star',
+    },
+    {
+        name: 'captains_dance',
+        displayName: 'Captain&#39s Dance',
+        artist: 'Marcus Warner',
+    },
+    {
+        name: 'numb',
+        displayName: 'Numb',
+        artist: 'Linkin Park',
+    },
+]
+
+
+//Check if playing
+let isPlaying = false;
+
+//Play
+function playSong () {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause')
+    music.play();
+}
+
+//Pause
+function pauseSong () {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play')
+    music.pause();
+}
+
+// Play or Pause event listener
+playBtn.addEventListener('click', () => isPlaying ? pauseSong() : playSong());
+
+//Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.scr = `music/${song.name}.mp3`;
+    image.src = `cover/${song.name}.jpg`;
+}
+
+//Current Song
+let songIndex = 0;
+
+//Prev Song
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+//Next Song
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+//On load - Select First Song
+loadSong(songs[songIndex]);
+
+//Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+
