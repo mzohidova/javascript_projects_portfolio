fb4074b486be60800caafe753865d08849746f0a
Matluba <mzohidova@mail.ru>
2021-01-27 23:40:32 +0300

    Typing Game - JS(2)

--- typing_game/script.js
+++ typing_game/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -42,6 +42,12 @@ let score = 0;
 //Init time
 let time = 10;
 
+//Focus on text on start
+text.focus();
+
+//Start countdown
+const timeInterval = setInterval(updateTime, 1000);
+
 //Generate random word from array
 function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)];
@@ -59,6 +65,30 @@ function updateScore() {
     scoreEl.innerHTML = score;
 }
 
+//Update time
+function updateTime() {
+    time--;
+    timeEl.innerHTML = time + 's';
+
+    if(time === 0) {
+        clearInterval(timeInterval);
+
+        //end game
+        gameOver();
+    }
+}
+
+//Game over, show end screen
+function gameOver() {
+    endgameEl.innerHTML = `
+        <h1>Time ran out</h1>
+        <p>Your final score is ${score}</p>
+        <button onclick="location.reload()">Reload</button>
+    `;
+
+    endgameEl.style.display = 'flex';
+}
+
 addWordToDOM();
 
 //Event listeners
@@ -71,6 +101,10 @@ text.addEventListener('input', e => {
 
         //Clear
         e.target.value = '';
+
+        time += 5;
+
+        updateTime();
     }
 })
 
