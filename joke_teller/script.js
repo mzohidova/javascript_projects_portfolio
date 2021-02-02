6fc2d3a3fe8910c671ed745d8b5f9fcf75912de0
Matluba <mzohidova@mail.ru>
2021-02-02 23:20:35 +0300

    Music Player - JS (4)

--- joke_teller/script.js
+++ joke_teller/script.js
@@ -1,2 +1,24 @@
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
+
+//Get jokes from JokeAPI (url: https://sv443.net/jokeapi/v2/)
+
+async function getJokes () {
+    let joke = '';
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setuo) {
+            joke = `${data.setup}...${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        console.log(joke);
+    } catch (error) {
+        //Catch errors
+        console.log('whoops', error)
+    }
+}
+
+getJokes();
