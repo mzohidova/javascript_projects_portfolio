b8b66776b096008003011968a01065dd11d5f9a5
Matluba <mzohidova@mail.ru>
2021-02-19 22:43:54 +0300

    Lyrics Search App - JS(3). The project doesn't show lyrics. Maybe due to some issues in HTML

--- lyrics_search_app/script.js
+++ lyrics_search_app/script.js
@@ -15,15 +15,8 @@ async function searchSongs (term) {
 
 //Show song and artist in DOM
 function showData(data) {
-    // let output = '';
-    //
-    // data.data.forEach(song => {output =+ `<li><span><strong>${song.artist.name}</strong> - ${song.title}</span><button class="btn" data-artist="${song.artist.name}"data-songtitle="${song.title}">Get Lyrics</li>`;})
-    //
-    // result.innerHTML = `<ul class="songs">${output}</ul>`;
-
     result.innerHTML = `<ul class="songs">${data.data.map(
-// language=HTML
-        song => `<li><span><strong>${song.artist.name}</strong> - ${song.title}</span><button class="btn" data-artist="${song.artist.name}"data-songtitle="${song.title}">Get Lyrics</li>`).join('')}</ul>`
+        song => `<li><span><strong>${song.artist.name}</strong> - ${song.title}</span><button class="btn" data-artist="${song.artist.name}"data-songtitle="${song.title}">Get Lyrics</button></li>`).join('')}</ul>`;
 
     if (data.prev || data.next) {
         more.innerHTML = `${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''}${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}`;
@@ -40,6 +33,18 @@ async function getMoreSons(url) {
     showData(data);
 }
 
+//Get lyrics for song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiUrl}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2><span>${lyrics}</span>`;
+
+    more.innerHTML = '';
+}
+
 //Event listener
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -51,4 +56,16 @@ form.addEventListener('submit', e => {
     } else {
         searchSongs(searchTerm);
     }
-})
+});
+
+//Get lyrics button click
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+
+    if (clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+
+        getLyrics(artist, songTitle);
+    }
+});
